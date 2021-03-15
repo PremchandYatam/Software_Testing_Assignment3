9fcfe66711dbc606baf84b000061dbe2a988683b
Premchand Naidu <premchands10@gmail.com>
2021-03-15 19:15:07 +0100

    Main file by prem

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    public Calculator() {
+
+    }
+    public int add(int a, int b){
+        return 0;
+    }
+    public int sub(int c,int d) {
+        return 0;
+    }
+    public int mul(int e,int f) {
+        return 0;
+    }
+    public int div(int g,int h) {
+        return 0;
+    }
+
+}
